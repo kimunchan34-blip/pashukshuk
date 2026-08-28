b7fada809ff9375e746e027daf8d4212bcdbfc2c
kimunchan34 <kimunchan34@gmail.com>
2026-08-28 17:42:16 +0900

    Use Firebase config fallback for static builds

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,22 +3,22 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey:
-    process.env.NEXT_PUBLIC_FIREBASE_API_KEY ??
+    process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
     "AIzaSyA4gzyFe9vpDHStWX4nK0t28ycGz73YrOc",
   authDomain:
-    process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ??
+    process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
     "pashukshuk-2e239.firebaseapp.com",
   projectId:
-    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ??
+    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ||
     "pashukshuk-2e239",
   storageBucket:
-    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ??
+    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||
     "pashukshuk-2e239.firebasestorage.app",
   messagingSenderId:
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ??
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||
     "1008200794336",
   appId:
-    process.env.NEXT_PUBLIC_FIREBASE_APP_ID ??
+    process.env.NEXT_PUBLIC_FIREBASE_APP_ID ||
     "1:1008200794336:web:dc9d5a65e32f399c9d5085",
 };
 
